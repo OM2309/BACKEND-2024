001a1c2d8a09a80dad9e01bcad724625e56d0596
anurag <23anuragsharma@gmail.com>
2024-01-28 20:22:41 +0530

    AsynHandler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server is listening on http://localhost:${process.env.PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB error: " + err);
+  });
